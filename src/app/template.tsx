8b7702d5c9e665c3befa5f83ddcec28804114821
JPSS14 <jp_scarabelli@hotmail.com>
2023-05-11 20:08:05 -0300

    Add global components import

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,5 +1,4 @@
-import { Header } from "./components/header/header";
-import { Intro } from "./components/intro/intro";
+import { Header, Intro } from "./components";
 
 export default function HomeTemplate({
   children,
