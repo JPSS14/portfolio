dfd723340549c9ad16eff0c078eed355486192ac
JPSS14 <jp_scarabelli@hotmail.com>
2024-04-01 16:27:25 -0300

    Add react-slick

--- src/app/components/project-list/project-list.tsx
+++ src/app/components/project-list/project-list.tsx
@@ -1,21 +1,58 @@
+"use client";
 import { ProjectCard } from "../project-card";
 import { ProjectCardType } from "../project-card/types";
 import styles from "./project-list.module.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useEffect, useState } from "react";
+import { settings } from "./settings";
 
 interface ProjectListProps {
   projectItems: ProjectCardType[];
 }
 
 export const ProjectList = ({ projectItems }: ProjectListProps) => {
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Adiciona um event listener para o evento de redimensionamento da janela
+    window.addEventListener("resize", handleResize);
+
+    // Chama o manipulador de redimensionamento uma vez para definir o estado inicial
+    handleResize();
+
+    // Remove o event listener quando o componente é desmontado
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <section className={styles.projectList__section}>
       <header className={styles.section__header}>
         <h1>Projetos</h1>
       </header>
       <div className={styles.projectList__container}>
-        {projectItems.map((item) => (
-          <ProjectCard projectItem={item} key={item.id} />
-        ))}
+        {windowWidth > 703 ? (
+          <>
+            {projectItems.map((item) => (
+              <ProjectCard projectItem={item} key={item.id} />
+            ))}
+          </>
+        ) : (
+          <div className={styles.projectList__container}>
+            <Slider {...settings}>
+              {projectItems.map((item) => (
+                <ProjectCard projectItem={item} key={item.id} />
+              ))}
+            </Slider>
+          </div>
+        )}
       </div>
     </section>
   );
---
+++ src/app/components/project-list/settings.ts
@@ -0,0 +1,80 @@
+export const settings = {
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 660,
+      settings: {
+        slidesToShow: 1.9,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 631,
+      settings: {
+        slidesToShow: 1.8,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 592,
+      settings: {
+        slidesToShow: 1.7,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 552,
+      settings: {
+        slidesToShow: 1.6,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 523,
+      settings: {
+        slidesToShow: 1.5,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 494,
+      settings: {
+        slidesToShow: 1.4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 468,
+      settings: {
+        slidesToShow: 1.3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 434,
+      settings: {
+        slidesToShow: 1.2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 410,
+      settings: {
+        slidesToShow: 1.15,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 390,
+      settings: {
+        slidesToShow: 1.1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
