c330543c8d30173c108163ca74fdd7b60f55085b
JPSS14 <jp_scarabelli@hotmail.com>
2024-03-31 22:17:46 -0300

    Add ProjectList component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,5 +2,6 @@ export { Header } from "./header";
 export { Intro } from "./intro";
 export { LozengeIcon } from "./lozenge-icon";
 export { ProfessionalXP } from "./professional-xp";
+export { ProjectList } from "./project-list";
 export { SkillCard } from "./skill-card";
 export { SkillList } from "./skill-list";
---
+++ src/app/components/project-list/project-list.tsx
@@ -0,0 +1,11 @@
+import styles from "./project-list.module.scss";
+
+export const ProjectList = () => {
+  return (
+    <section className={styles.projectList__section}>
+      <header className={styles.section__header}>
+        <h1>Projetos</h1>
+      </header>
+    </section>
+  );
+};
