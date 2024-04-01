6580c65369eaea9969a1d3adc63efa8be90fb267
JPSS14 <jp_scarabelli@hotmail.com>
2024-04-01 16:28:34 -0300

    Add ProjectList and ProfessionalXP in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import styles from "./page.module.scss";
-import { ProfessionalXP, SkillList } from "./components";
+import { ProfessionalXP, ProjectList, SkillList } from "./components";
+import { projectList } from "@/constants/projectList";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <SkillList />
+      <ProjectList projectItems={projectList} />
       {/* <Link href="/projects">Projects</Link> */}
       <ProfessionalXP />
     </main>
